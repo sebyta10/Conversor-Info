526a45637ee89789268a29959114a523a692915f
sebyta10 <rsebyfigue@gmail.com>
2025-10-29 21:32:23 -0300

    commit inicial

---
+++ ConversionesMatematicas.py
@@ -0,0 +1,52 @@
+#COMVERSIONES MATEMÁTICAS
+#PARA LA MASA
+#Para convertir del SI(kg) a los demas sistemas:
+def kg_a_g(valor):
+    return valor*1000 #convierte de kilogramos a gramos
+
+def kg_a_lb(valor):
+    return valor*2.20462 #convierte de kilogramos a libras
+
+def kg_a_utm(valor):
+    return valor/ 9.80665 #convierte de kilogramos a utm
+
+#Para convertir del CGS(g) a los demas sistemas:
+def g_a_kg(valor):
+    return valor/1000 #convierte de gramo a kilogramo
+
+def g_a_lb(valor):
+    return valor/453.592 #convierte de gramo a libras
+
+def g_a_utm(valor):
+    return valor/9806.65 #convierte de gramo a utm
+
+#Para convertir del US(lb) a los demas sistemas:
+def lb_a_kg(valor):
+    return valor/2.20462 #convierte de libra a kilogramo
+
+def lb_a_g(valor):
+    return valor*453.592 #convierte de libra a gramo
+
+def lb_a_utm(valor):
+    return valor/21.6186 #convierte de libra a utm
+
+#Para convertir de Técnico(utm) a los demas sistemas:
+def utm_a_kg(valor):
+    return valor*9.80665 #convierte de utm a kilogramo
+
+def utm_a_g(valor):
+    return valor*9806.65 #convierte de utm a gramo
+
+def utm_a_lb(valor):
+    return valor*21.6186 #convierte de utm a libra
+
+#----------------------------------------------------
+
+#PARA LA LONGITUD
+#PARA LA FUERZA
+#PARA LA TEMPERATURA
+#PARA LA ENERGIA
+
+
+
+    
